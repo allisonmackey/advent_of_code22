5a4870ca212392907dcdcc0e39f75a34b5b91aa3
Allison Mackey <a.mackey693@gmail.com>
2022-12-11 00:00:25 -0500

    day_two: refactor for part2 🍾

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -4,7 +4,9 @@ def scrub_and_transform_data(data)
       .gsub(/\n/, ",")
       .split(",")
       .map { |v| v.split(" ") }
+end
 
+def puzzle_one(data)
   cheat_sheet = {
     ["A", "X"] => 4, # rock vs rock = 1 + 3 -> tie
     ["A", "Y"] => 8, # rock vs paper = 2 + 6 -> win
@@ -17,11 +19,29 @@ def scrub_and_transform_data(data)
     ["C", "Z"] => 6, # scissors vs scissors 3 + 3 = -> tie
   }
 
-
-  clean_data.map {|d| cheat_sheet[d] }
+  scrub_and_transform_data(data).map {|d| cheat_sheet[d] }
 end
 
-puts scrub_and_transform_data(File.read("input.txt")).sum
+def puzzle_two(data)
+  cheat_sheet = {
+    ["A", "X"] => 3, # rock vs scissors = 3 + 0 -> lose
+    ["A", "Y"] => 4, # rock vs rock = 1 + 3 -> tie
+    ["A", "Z"] => 8, # rock vs paper = 2 + 6 -> win
+    ["B", "X"] => 1, # paper vs rock = 1 + 0 -> lose
+    ["B", "Y"] => 5, # paper vs paper = 2 + 3 -> tie
+    ["B", "Z"] => 9, # paper vs scissors = 3 + 6 -> win
+    ["C", "X"] => 2, # scissors vs paper 2 + 0 = -> lose
+    ["C", "Y"] => 6, # scissors vs scissors 3 + 3 = -> tie
+    ["C", "Z"] => 7, # scissors vs rock = 1 + 6 -> win
+  }
+
+  scrub_and_transform_data(data).map {|d| cheat_sheet[d] }
+end
+
+file_data = File.read("input.txt")
+
+puts "Puzzle 1: #{ puzzle_one(file_data).sum }"
+puts "Puzzle 2: #{ puzzle_two(file_data).sum }"
 
 
 
